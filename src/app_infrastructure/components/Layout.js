1755a0bbc5a37ac2f0995f79d5456a5ec05e7d4c
MateDawid <mateusiakdawid@gmail.com>
2024-11-25 12:04:51 +0100

    signup: Layout component

--- src/app_infrastructure/components/Layout.js
+++ src/app_infrastructure/components/Layout.js
@@ -1,12 +1,18 @@
-import {Outlet} from "react-router-dom";
 import React from "react";
-import {Button} from "@mui/material";
+import {Outlet} from "react-router-dom";
+import Navbar from "./Navbar";
+import {Container} from "@mui/material";
 
+/**
+ * Layout component holds base elements of displayed pages.
+ */
 function Layout() {
     return (
         <>
-            <Button variant="contained">BUDGETORY</Button>
-            <Outlet/>
+            <Navbar/>
+            <Container>
+                <Outlet/>
+            </Container>
         </>
     );
 }
