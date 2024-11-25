15e69e87a0cb293f2d06cf44dd72f769947fba4d
JE01TD <dawid.mateusiak@ing.com>
2024-11-25 12:05:05 +0100

    signup: App component

--- src/app_infrastructure/components/App.js
+++ src/app_infrastructure/components/App.js
@@ -3,17 +3,21 @@ import {Route, Routes} from 'react-router-dom';
 
 import '../styles/App.css';
 import Layout from "./Layout";
+import LandingPage from "./LandingPage";
+import LoginForm from "../../app_users/components/LoginForm";
+import RegisterForm from "../../app_users/components/RegisterForm";
 
+/**
+ * App component handles routing of application.
+ */
 function App() {
     return (
         <Routes>
-            <Route
-                path='/'
-                element={
-                    <Layout/>
-                }
-            >
+            <Route path='/' element={<Layout/>}>
+                <Route index element={<LandingPage/>}/>
             </Route>
+            <Route path='login' element={<LoginForm/>}/>
+            <Route path='register' element={<RegisterForm/>}/>
         </Routes>
     );
 }
