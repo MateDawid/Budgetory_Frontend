dbea55226f486d7ab814e8bcadb423ae1fa50a51
MateDawid <mateusiakdawid@gmail.com>
2024-11-25 11:56:18 +0100

    signup: Register service and form update

--- src/app_users/services/RegisterService.js
+++ src/app_users/services/RegisterService.js
@@ -3,13 +3,14 @@ import axios from "axios";
 /**
  * Function to register user with provided credentials.
  * @param {string} email - User email.
- * @param {string} password - User password.
+ * @param {string} password_1 - User password 1.
+ * @param {string} password_2 - User password 2.
  * @return {Promise} - Request response and isError boolean value.
  */
-export const registerUser = async (email, password) => {
-    const url = `${process.env.REACT_APP_BASE_URL}/api/users/create/`;
+export const registerUser = async (email, password_1, password_2) => {
+    const url = `${process.env.REACT_APP_BASE_URL}/api/users/register/`;
     try {
-        const response = await axios.post(url, {'email': email, 'name': email, 'password': password});
+        const response = await axios.post(url, {'email': email, 'password_1': password_1, 'password_2': password_2});
         return {response, isError: false};
     } catch (error) {
         return {response: error, isError: true};
