3bd42193af91741a91a279cb7df20c01165009ec
MateDawid <mateusiakdawid@gmail.com>
2024-11-26 08:38:49 +0100

    signup: Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    // env: {
+    //   credentials: {
+    //     username: "cypress@example.com",
+    //     password: "pAssw0rd",
+    //   },
+    // },
   },
 });
