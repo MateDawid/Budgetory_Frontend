455fe8abae362af81a429599da478fa448fa0340
JE01TD <dawid.mateusiak@ing.com>
2024-11-29 14:42:59 +0100

    signup: Cypress and faker updated

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import axios from "axios";
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
