b955ba857bd09a063b6174e522cb87bc4a356a72
JE01TD <dawid.mateusiak@ing.com>
2024-12-01 08:03:08 +0100

    signup: E2E tests for RegisterForm

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    backendUrl: "http://localhost:8000"
     // env: {
     //   credentials: {
     //     username: "cypress@example.com",
