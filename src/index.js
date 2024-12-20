74be1f35c4e804184bf9773f5573053f4230eb6e
JE01TD <dawid.mateusiak@ing.com>
2024-12-20 08:43:31 +0100

    budgets: proper React root in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import '@fontsource/roboto/400.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './app_infrastructure/components/App';
-import reportWebVitals from './reportWebVitals';
 import axios from "axios";
+import { createRoot } from 'react-dom/client';
+
 
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <HashRouter>
@@ -18,8 +19,3 @@ root.render(
     </HashRouter>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
