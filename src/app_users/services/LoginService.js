9c1f53bec97243cf2de492ef9cda26451632a2ad
MateDawid <mateusiakdawid@gmail.com>
2024-12-25 10:58:26 +0100

    budgets: calculateTokenExpirationTime

--- src/app_users/services/LoginService.js
+++ src/app_users/services/LoginService.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 
 /**
+ * Function to calculate token expiration time.
+ * @param {string} tokenLifetime - Number of seconds after which token expires.
+ * @return {string} - Time when token expires.
+ */
+const calculateTokenExpirationTime = (tokenLifetime) => {
+    const numberTokenLifetime = parseInt(tokenLifetime)
+    return (new Date().getTime() + numberTokenLifetime * 1000).toString()
+}
+
+/**
  * Function to log in User with provided credentials. Adds token key in localStorage.
  * @param {string} email - User email.
  * @param {string} password - User password.
@@ -10,14 +20,13 @@ export const logIn = async (email, password) => {
     const url = `${process.env.REACT_APP_BACKEND_URL}/api/users/login/`;
     try {
         const response = await axios.post(url, {'email': email, 'password': password});
-        // TODO - handle invalid dates -> navigate to error page
         window.localStorage.setItem(
             'budgetory.accessTokenExpiresIn',
-            (new Date().getTime() + parseInt(process.env.REACT_APP_ACCESS_TOKEN_LIFETIME) * 1000).toString()
+            calculateTokenExpirationTime(process.env.REACT_APP_ACCESS_TOKEN_LIFETIME)
         )
         window.localStorage.setItem(
             'budgetory.refreshTokenExpiresIn',
-            (new Date().getTime() + parseInt(process.env.REACT_APP_REFRESH_TOKEN_LIFETIME) * 1000).toString()
+            calculateTokenExpirationTime(process.env.REACT_APP_REFRESH_TOKEN_LIFETIME)
         )
         window.localStorage.setItem('budgetory.accessToken', response.data.access);
         window.localStorage.setItem('budgetory.refreshToken', response.data.refresh);
@@ -43,7 +52,7 @@ export const removeTokens = () => {
  */
 const getAccessToken = () => {
     if (window.localStorage.getItem('budgetory.accessTokenExpiresIn') <= (new Date()).getTime()) {
-        refreshToken()
+        refreshToken().then()
     }
     return window.localStorage.getItem('budgetory.accessToken')
 }
