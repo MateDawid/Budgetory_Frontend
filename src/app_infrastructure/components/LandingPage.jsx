f23c5458116bbab4d6dac98cee54fa633ba84072
JE01TD <dawid.mateusiak@ing.com>
2025-01-01 10:53:22 +0100

    budgets: LandingPage update

--- src/app_infrastructure/components/LandingPage.jsx
+++ src/app_infrastructure/components/LandingPage.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import {isLoggedIn} from "../../app_users/services/LoginService";
-import {Navigate} from "react-router-dom";
 
 /**
  * LandingPage component displays home page of application.
  */
 function LandingPage() {
-    if (!isLoggedIn()) {
-        return <Navigate to='/login'/>;
-    }
     return (<><h1>LANDING PAGE</h1></>);
 }
 
