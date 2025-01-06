bd1b719d8eb88ad680810c9c8374feb6128dd693
JE01TD <dawid.mateusiak@ing.com>
2025-01-06 08:57:23 +0100

    budgets: Service for Budget list fetching

---
+++ src/budgets/services/BudgetService.js
@@ -0,0 +1,16 @@
+import {getAccessToken} from "../../app_users/services/LoginService";
+
+/**
+ * Function to get list of User Budgets.
+ * @param {object} paginationModel - paginationModel object with page number and page size.
+ * @return {object} - JSON data with API response.
+ */
+export const getBudgetList = async (paginationModel) => {
+    const token = await getAccessToken()
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/budgets/?` + new URLSearchParams({
+        page: paginationModel.page + 1,
+        page_size: paginationModel.pageSize
+    });
+    const response = await fetch(url, {method: "GET", headers: {Authorization: `Bearer ${token}`}})
+    return await response.json();
+};
