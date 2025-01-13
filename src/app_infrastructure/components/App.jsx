3a4e9556b101ce6d1467f0f0e3930736233ce851
JE01TD <dawid.mateusiak@ing.com>
2025-01-13 12:06:29 +0100

    budgets: AlertContext

--- src/app_infrastructure/components/App.jsx
+++ src/app_infrastructure/components/App.jsx
@@ -8,24 +8,28 @@ import LoginForm from "../../app_users/pages/LoginForm";
 import RegisterForm from "../../app_users/pages/RegisterForm";
 import BudgetList from "../../budgets/pages/BudgetList";
 import BudgetDetail from "../../budgets/pages/BudgetDetail";
+import BudgetAdd from "../../budgets/pages/BudgetAdd";
+import {AlertProvider} from "./AlertContext";
 
 /**
  * App component handles routing of application.
  */
 function App() {
     return (
-        <Routes>
-            <Route path='/' element={<BasePage/>}>
-                <Route index element={<LandingPage/>}/>
-                <Route path='budgets'>
-                    <Route index element={<BudgetList/>}/>
-                    {/*<Route path='add' element={<BudgetAdd/>}/>*/}
-                    <Route path=':budgetId' element={<BudgetDetail/>}/>
+        <AlertProvider>
+            <Routes>
+                <Route path='/' element={<BasePage/>}>
+                    <Route index element={<LandingPage/>}/>
+                    <Route path='budgets'>
+                        <Route index element={<BudgetList/>}/>
+                        <Route path='add' element={<BudgetAdd/>}/>
+                        <Route path=':budgetId' element={<BudgetDetail/>}/>
+                    </Route>
                 </Route>
-            </Route>
-            <Route path='login' element={<LoginForm/>}/>
-            <Route path='register' element={<RegisterForm/>}/>
-        </Routes>
+                <Route path='login' element={<LoginForm/>}/>
+                <Route path='register' element={<RegisterForm/>}/>
+            </Routes>
+        </AlertProvider>
     );
 }
 
