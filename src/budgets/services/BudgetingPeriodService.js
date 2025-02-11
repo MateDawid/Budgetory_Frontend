769c091a77ad07649384259110c1c359f44e7078
MateDawid <mateusiakdawid@gmail.com>
2025-02-11 11:27:32 +0100

    periods: Editing rows in DataGrid

--- src/budgets/services/BudgetingPeriodService.js
+++ src/budgets/services/BudgetingPeriodService.js
@@ -1,4 +1,5 @@
 import {getAccessToken} from "../../app_users/services/LoginService";
+import ApiError from "../../app_infrastructure/utils/ApiError";
 
 /**
  * Function to get list of Budget BudgetingPeriods.
@@ -16,18 +17,6 @@ export const getBudgetingPeriodList = async (budgetId, paginationModel) => {
     return await response.json();
 };
 
-// /**
-//  * Function to get details of single Budget.
-//  * @param {string} budgetId - id of Budget object.
-//  * @return {object} - JSON data with API response.
-//  */
-// export const getBudgetDetail = async (budgetId) => {
-//     const token = await getAccessToken()
-//     const url = `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${budgetId}`
-//     const response = await fetch(url, {method: "GET", headers: {Authorization: `Bearer ${token}`}})
-//     return await response.json();
-// };
-//
 // /**
 //  * Function to create Budget.
 //  * @param {object} budgetData - Created Budget data.
@@ -56,46 +45,45 @@ export const getBudgetingPeriodList = async (budgetId, paginationModel) => {
 //         }
 //     }
 // ;
-//
-// /**
-//  * Function to update single Budget.
-//  * @param {string} budgetId - id of Budget object.
-//  * @param {string} apiFieldName - API field name for field value.
-//  * @param {object} newValue - New value for updated field.
-//  * @return {object} - JSON data with API response.
-//  */
-// export const updateBudget = async (budgetId, apiFieldName, newValue) => {
-//     let dataErrorRaised = false
-//     try {
-//         const token = await getAccessToken()
-//         const url = `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${budgetId}/`
-//         const requestOptions = {
-//             method: "PATCH",
-//             headers: {
-//                 "Authorization": `Bearer ${token}`,
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({[apiFieldName]: newValue})
-//         }
-//         const response = await fetch(url, requestOptions)
-//         if (!response.ok) {
-//             const data = await response.json()
-//             dataErrorRaised = true
-//             throw new Error(data.detail[apiFieldName][0]);
-//         }
-//         return await response.json();
-//     }
-//     catch (error) {
-//         if (dataErrorRaised) {
-//             throw error;
-//         } else {
-//             throw new Error("Unexpected error occurred.");
-//         }
-//
-//     }
-// };
-//
-//
+
+/**
+ * Function to update single BudgetingPeriod.
+ * @param {string} budgetId - id of Budget object.
+ * @param {object} updatedObject - Payload for API call with updated object values.
+ * @return {object} - JSON data with API response.
+ */
+export const updateBudgetingPeriod = async (budgetId, updatedObject) => {
+    let dataErrorRaised = false
+    try {
+        const token = await getAccessToken()
+        const url = `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${budgetId}/periods/${updatedObject["id"]}/`
+        const requestOptions = {
+            method: "PATCH",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedObject)
+        }
+        const response = await fetch(url, requestOptions)
+        if (!response.ok) {
+            const data = await response.json()
+            dataErrorRaised = true
+            throw new ApiError('Invalid data', data);
+        }
+        return await response.json();
+    }
+    catch (error) {
+        if (dataErrorRaised) {
+            throw error;
+        } else {
+            throw new Error("Unexpected error occurred.");
+        }
+
+    }
+};
+
+
 // /**
 //  * Function to delete single Budget.
 //  * @param {string} budgetId - id of Budget object.
