60484b12ae32a7251fd79fe59fe188b347d8f7a3
MateDawid <mateusiakdawid@gmail.com>
2025-02-19 13:42:44 +0100

    periods: ApiInputFormatters extended with handling column type boolean

--- src/app_infrastructure/utils/ApiInputFormatters.js
+++ src/app_infrastructure/utils/ApiInputFormatters.js
@@ -1,21 +1,20 @@
 /**
  * Function for converting Date object to string in format YYYY-MM-DD.
- * @param {Date} date - Date object.
- * @return {string} - Date object converted to string in format YYYY-MM-DD.
- */
-export function convertDateToApiFormat(date) {
-    return date.toLocaleDateString('en-CA');
-}
-
-/**
- * Function for converting Date object to string in format YYYY-MM-DD.
  * @param {object} apiInput - DataGrid row.
+ * @param {object} columns - DataGrid columns.
  * @return {object} - Formatted DataGrid row.
  */
-export function prepareApiInput(apiInput) {
+export function prepareApiInput(apiInput, columns) {
     Object.keys(apiInput).forEach(key => {
-        if (apiInput[key] instanceof Date) {
-            apiInput[key] = convertDateToApiFormat(apiInput[key])
+        const column = columns.find(column => column.field === key)
+        if (!column) {
+            return;
+        }
+        if (column.type === 'date' && apiInput[key] instanceof Date) {
+            apiInput[key] = apiInput[key].toLocaleDateString('en-CA');
+        }
+        else if (column.type === 'boolean' && typeof apiInput[key] !== 'boolean'){
+            apiInput[key] = !!apiInput[key]
         }
     });
     return apiInput
