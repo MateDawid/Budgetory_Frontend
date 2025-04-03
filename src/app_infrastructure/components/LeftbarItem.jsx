88cb38591a6b201e0b30b60b261cb966dca4a71a
MateDawid <mateusiakdawid@gmail.com>
2025-04-03 15:24:21 +0200

    period_rework: StyledListItemButton and StyledListItemIcon

--- src/app_infrastructure/components/LeftbarItem.jsx
+++ src/app_infrastructure/components/LeftbarItem.jsx
@@ -3,20 +3,19 @@ import {Link} from "@mui/material";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import * as React from "react";
+import {styled} from "@mui/material/styles";
 
-const iconComponentStyling = {color: '#BD0000'}
-const listItemButtonStyling = [
-    {minHeight: 48, px: 2.5},
-    open ? {justifyContent: 'initial'} : {justifyContent: 'center'},
-]
-const listItemIconStyling = [
-    {minWidth: 0, justifyContent: 'center',},
-    open ? {mr: 3} : {mr: 'auto'},
-]
-const listItemTextStyling = [
-    open ? {opacity: 1,} : {opacity: 0,},
-    {color: '#FFFFFF'}
-]
+const StyledListItemButton = styled(ListItemButton)({
+    justifyContent: 'center',
+    minHeight: 48,
+    px: 2.5
+})
+
+const StyledListItemIcon = styled(ListItemIcon)({
+    justifyContent: 'center',
+    minWidth: 0,
+    marginRight: 15
+})
 
 
 /**
@@ -24,12 +23,12 @@ const listItemTextStyling = [
  */
 const LeftbarItem = ({url, displayText, icon}) => {
     return (
-        <ListItemButton component={Link} to={url} sx={listItemButtonStyling}>
-            <ListItemIcon sx={listItemIconStyling}>
-                {React.cloneElement(icon, { style: iconComponentStyling })}
-            </ListItemIcon>
-            <ListItemText primary={displayText} sx={listItemTextStyling}/>
-        </ListItemButton>
+        <StyledListItemButton component={Link} to={url}>
+            <StyledListItemIcon>
+                {React.cloneElement(icon, { style: {color: '#BD0000'} })}
+            </StyledListItemIcon>
+            <ListItemText primary={displayText} sx={{color: '#FFFFFF'}}/>
+        </StyledListItemButton>
     )
 }
 
