dc3ae434d61a5edb36bd99d514bae9b0751e4fa4
MateDawid <mateusiakdawid@gmail.com>
2025-04-10 14:56:57 +0200

    period_rework: StyledButton component

--- src/app_infrastructure/components/StyledButton.jsx
+++ src/app_infrastructure/components/StyledButton.jsx
@@ -1,15 +1,28 @@
-import {styled} from "@mui/material/styles";
-import {Button} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { Button } from "@mui/material";
 
-const StyledButton = styled(Button)({
-    backgroundColor: "#FFFFFF",
+const StyledButton = styled(Button)(({ variant }) => ({
+    ...(variant === 'contained' && {
+    backgroundColor: "#BD0000",
+    color: "#FFFFFF",
+        border: 1,
+    borderColor: "#FFFFFF",
+    '&:hover': {
+      backgroundColor: "#FFFFFF",
+      color: "#BD0000",
+      borderColor: "#BD0000"
+    },
+  }),
+  ...(variant === 'outlined' && {
+    backgroundColor: "transparent",
     color: "#BD0000",
     borderColor: "#BD0000",
     '&:hover': {
-        backgroundColor: "#BD0000",
-        color: "#FFFFFF",
-        borderColor: "#FFFFFF"
+      backgroundColor: "#BD0000",
+      color: "#FFFFFF",
+      borderColor: "#FFFFFF"
     },
-})
+  }),
+}));
 
 export default StyledButton;
