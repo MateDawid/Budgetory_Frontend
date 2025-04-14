6ccab8c88d6a00d63903016304df526add71f21c
MateDawid <mateusiakdawid@gmail.com>
2025-04-14 10:51:46 +0200

    budgets_rework: Fetching Budget Deposits in Rightbar

--- src/app_infrastructure/components/RightbarItem.jsx
+++ src/app_infrastructure/components/RightbarItem.jsx
@@ -1,18 +1,24 @@
 import {ListItem, Stack, Typography, ListItemText} from "@mui/material";
 import * as React from "react";
+import {useContext} from "react";
+import {BudgetContext} from "./BudgetContext";
 
 
 /**
  * RightbarItem component to display single Deposit in Rightbar.
  */
 const RightbarItem = (deposit) => {
+    const {contextBudgetCurrency} = useContext(BudgetContext);
+
     return (
         <ListItem>
             <ListItemText primary={
                 <React.Fragment>
-                    <Stack direction="row" justifyContent="space-between" alignItems="center">
+                    <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={3}>
                         <Typography>{deposit.deposit.name}</Typography>
-                        <Typography>{deposit.deposit.balance}</Typography>
+                        <Typography sx={{fontWeight: "bold", color: "#BD0000"}}>
+                            {deposit.deposit.balance}&nbsp;{contextBudgetCurrency}
+                        </Typography>
                     </Stack>
                 </React.Fragment>
             }/>
