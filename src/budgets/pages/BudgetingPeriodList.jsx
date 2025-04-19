def9fec4fc2df2c9a2397a76b578744a4af6352d
MateDawid <mateusiakdawid@gmail.com>
2025-04-19 12:14:58 +0200

    periods_rework: BudgetingPeriodList component

--- src/budgets/pages/BudgetingPeriodList.jsx
+++ src/budgets/pages/BudgetingPeriodList.jsx
@@ -1,11 +1,13 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Typography from "@mui/material/Typography";
-import {Paper} from "@mui/material";
+import {Box, Paper, Stack} from "@mui/material";
 import Divider from "@mui/material/Divider";
 import Alert from '@mui/material/Alert';
 import {AlertContext} from "../../app_infrastructure/components/AlertContext";
-import DataTable from "../../app_infrastructure/components/DataTable";
 import {BudgetContext} from "../../app_infrastructure/components/BudgetContext";
+import {getApiObjectsList} from "../../app_infrastructure/services/APIService";
+import BudgetingPeriodCard from "../components/BudgetingPeriodCard";
+import BudgetingPeriodAddButton from "../components/BudgetingPeriodAddButton";
 
 
 /**
@@ -15,87 +17,44 @@ export default function BudgetingPeriodList() {
     const {contextBudgetId} = useContext(BudgetContext);
     const apiUrl = `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${contextBudgetId}/periods/`
     const {alert, setAlert} = useContext(AlertContext);
-    const [statusOptions, setStatusOptions] = useState([]);
-    const columns = [
-        {
-            field: 'name',
-            type: 'string',
-            headerName: 'Name',
-            flex: 2,
-            filterable: true,
-            sortable: true,
-            editable: true,
-        },
-        {
-            field: 'status',
-            type: 'singleSelect',
-            headerName: 'Status',
-            flex: 2,
-            filterable: true,
-            sortable: true,
-            editable: true,
-            valueOptions: statusOptions,
-            valueOptionsSetter: setStatusOptions,
-            valueOptionsApiUrl: `${process.env.REACT_APP_BACKEND_URL}/api/budgets/periods/statuses`
-        },
-        {
-            field: 'date_start',
-            type: 'date',
-            headerName: 'Date start',
-            flex: 2,
-            filterable: true,
-            sortable: true,
-            editable: true,
-            valueGetter: (value) => {
-                return new Date(value);
-            },
-            valueFormatter: (value) => {
-                try {
-                    return value.toLocaleDateString('en-CA')
-                } catch (error) {
-                    return value
-                }
-            },
-        },
-        {
-            field: 'date_end',
-            type: 'date',
-            headerName: 'Date end',
-            flex: 2,
-            filterable: true,
-            sortable: true,
-            editable: true,
-            valueGetter: (value) => {
-                return new Date(value);
-            },
-            valueFormatter: (value) => {
-                try {
-                    return value.toLocaleDateString('en-CA')
-                } catch (error) {
-                    return value
-                }
-            },
-        },
-    ]
+    const [addedObjectId, setAddedObjectId] = useState(null);
+    const [deletedObjectId, setDeletedObjectId] = useState(null);
+    const [objects, setObjects] = useState([]);
+
+    /**
+     * Fetches Budgeting Periods list from API.
+     */
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+                const listResponse = await getApiObjectsList(apiUrl)
+                setObjects(listResponse);
+            } catch (err) {
+                setAlert({type: 'error', message: "Failed to load Periods."});
+            }
+        }
+        loadData();
+    }, [contextBudgetId, addedObjectId, deletedObjectId]);
 
     return (
-        <>
-            <Paper elevation={24} sx={{
-                padding: 2, bgColor: "#F1F1F1", width: '100%', maxWidth: '100%',
-                '& .datagrid--header': {
-                    backgroundColor: '#BD0000',
-                }
-            }}>
-                <Typography variant="h4" gutterBottom
+        <Paper elevation={24} sx={{
+            padding: 2, bgColor: "#F1F1F1"
+        }}>
+            <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1} mb={1}>
+                <Typography variant="h4"
                             sx={{display: 'block', color: '#BD0000'}}>Periods</Typography>
-                <Divider/>
-                {alert && <Alert sx={{marginTop: 2, whiteSpace: 'pre-wrap'}} severity={alert.type}
-                                 onClose={() => setAlert(null)}>{alert.message}</Alert>}
-                <DataTable
-                    columns={columns}
-                    apiUrl={apiUrl}
-                />
-            </Paper>
-        </>
+                <BudgetingPeriodAddButton apiUrl={apiUrl} setAddedObjectId={setAddedObjectId}/>
+            </Stack>
+            <Divider/>
+            {alert && <Alert sx={{marginTop: 2, whiteSpace: 'pre-wrap'}} severity={alert.type}
+                             onClose={() => setAlert(null)}>{alert.message}</Alert>}
+            <Box sx={{display: "flex", flexWrap: 'wrap', justifyContent: 'flex-start'}}>
+                {objects.map(object => (
+                    <Box key={object.id} sx={{width: 330, m: 1}}>
+                        <BudgetingPeriodCard apiUrl={apiUrl} object={object} setDeletedObjectId={setDeletedObjectId}/>
+                    </Box>
+                ))}
+            </Box>
+        </Paper>
     );
 }
