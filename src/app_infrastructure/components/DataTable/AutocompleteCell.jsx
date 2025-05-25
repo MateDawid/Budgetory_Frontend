f990f37324fef0115466fe0823c70d70610f37e0
MateDawid <mateusiakdawid@gmail.com>
2025-05-25 08:16:03 +0200

    transfers_rework: AutocompleteCell

---
+++ src/app_infrastructure/components/DataTable/AutocompleteCell.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Autocomplete} from "@mui/material";
+import StyledTextField from "../StyledTextField";
+import {useGridApiContext} from "@mui/x-data-grid";
+
+
+/**
+ * AutocompleteCell component to display Autocomplete field in DataGrid cell.
+ * @param {number} id - Edited object id.
+ * @param {string} field - Edited field name.
+ * @param {any} value - Current value of field.
+ * @param {array} options - Options to be selected in field.
+ * @param {string} labelField - Field containing label to be displayed for options.
+ */
+export default function AutocompleteCell({id, field, value, options, labelField}) {
+    const apiRef = useGridApiContext();
+
+    /**
+     * AutocompleteCell component to display Autocomplete field in DataGrid cell.
+     * @param {object} _ - Change event.
+     * @param {number} newValue - New value for field.
+     */
+    const handleValueChange = (_, newValue) => {
+        apiRef.current.setEditCellValue({
+            id,
+            field,
+            value: typeof newValue === 'string' ? value : newValue?.value || '',
+        });
+    };
+
+    return (
+        <Autocomplete
+            value={options.find(option => option.value === value) || null}
+            options={options}
+            fullWidth
+            getOptionLabel={(selectedOption) => {
+                if (typeof selectedOption === 'object') {
+                    return selectedOption[labelField]
+                } else if (typeof selectedOption === 'number') {
+                    const displayOption = options.find(option => option.id === selectedOption)
+                    if (displayOption) {
+                        return displayOption[labelField]
+                    } else {
+                        console.error(`ERROR: Label for selected option not found - check "labelField" definition.`)
+                        return '?'
+                    }
+                }
+                return ""
+            }}
+            isOptionEqualToValue={(option, value) => {
+                if (option && value) {
+                    return option.value === value;
+                }
+                return false;
+            }}
+            onChange={handleValueChange}
+            renderInput={(params) => <StyledTextField {...params} />}
+        />
+    )
+}
