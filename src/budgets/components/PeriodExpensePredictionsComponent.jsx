f468c60cd68e21699c67cf2a213ac4ce8c109c98
MateDawid <mateusiakdawid@gmail.com>
2025-07-25 11:23:51 +0200

    predictions_in_periods: Mocked API call for setUserPeriodResults

--- src/budgets/components/PeriodExpensePredictionsComponent.jsx
+++ src/budgets/components/PeriodExpensePredictionsComponent.jsx
@@ -5,6 +5,7 @@ import { BudgetContext } from '../../app_infrastructure/store/BudgetContext';
 import { getApiObjectsList } from '../../app_infrastructure/services/APIService';
 import { ExpensePredictionCardComponent } from './ExpensePredictionCardComponent';
 import PeriodStatuses from '../utils/PeriodStatuses';
+import { UserPeriodResultComponent } from './UserPeriodResultComponent';
 
 const PeriodExpensePredictionsComponent = ({ periodId, periodStatus }) => {
     /*
@@ -18,6 +19,7 @@ const PeriodExpensePredictionsComponent = ({ periodId, periodStatus }) => {
     const apiUrl = `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${contextBudgetId}/expense_predictions/?period=${periodId}`
     const [categoryOptions, setCategoryOptions] = useState([]);
     const [periodPredictions, setPeriodPredictions] = useState([]);
+    const [userPeriodResults, setUserPeriodResults] = useState([]);
     const [alert, setAlert] = useState(null);
 
     const createFields = {
@@ -61,7 +63,61 @@ const PeriodExpensePredictionsComponent = ({ periodId, periodStatus }) => {
             const predictionsResponse = await getApiObjectsList(apiUrl)
             setPeriodPredictions(predictionsResponse);
         }
+        async function getUsersPeriodResults() {
+            // TODO - API call
+            setUserPeriodResults([
+                {
+                    user_id: '1',
+                    user_username: 'username1',
+                    period_id: '?',
+                    predictions_sum: 100,
+                    period_balance: 110,
+                    period_expenses: 90,
+                },
+                {
+                    user_id: '2',
+                    user_username: 'username2',
+                    period_id: '?',
+                    predictions_sum: 100,
+                    period_balance: 90,
+                    period_expenses: 110,
+                },
+                {
+                    user_id: '3',
+                    user_username: 'username2',
+                    period_id: '?',
+                    predictions_sum: 200,
+                    period_balance: 90,
+                    period_expenses: 450,
+                },
+                                {
+                    user_id: '4',
+                    user_username: 'username2',
+                    period_id: '?',
+                    predictions_sum: 0,
+                    period_balance: 0,
+                    period_expenses: 0,
+                },
+                                                {
+                    user_id: '5',
+                    user_username: 'username2',
+                    period_id: '?',
+                    predictions_sum: 1000,
+                    period_balance: 1000,
+                    period_expenses: 1000,
+                },
+                                                {
+                    user_id: '6',
+                    user_username: 'username2',
+                    period_id: '?',
+                    predictions_sum: 10000,
+                    period_balance: 10000,
+                    period_expenses: 10000,
+                },
+            ]);
+        }
         getPredictions();
+        getUsersPeriodResults();
     }, [contextBudgetId, objectChange]);
 
     /**
@@ -78,9 +134,20 @@ const PeriodExpensePredictionsComponent = ({ periodId, periodStatus }) => {
 
     return (
         <Box sx={{ marginTop: 2 }}>
-            <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
+            {/* Users results*/}
+            {userPeriodResults.length > 0 && <Typography variant="h5" sx={{ display: 'block', color: '#BD0000' }}>Users results</Typography>}
+            <Box sx={{ display: "flex", flexWrap: 'wrap', justifyContent: 'flex-start' }}>
+                {userPeriodResults.map((userPeriodResult) => (
+                    <UserPeriodResultComponent
+                        key={userPeriodResult.user_id}
+                        userPeriodResult={userPeriodResult}
+                    />)
+                )}
+            </Box>
+            {/* Expense predictions */}
+            <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1} mt={2}>
                 <Typography variant="h5" sx={{ display: 'block', color: '#BD0000' }}>Expense predictions</Typography>
-                <CreateButton fields={createFields} objectType={"Expense prediction"} apiUrl={apiUrl} customSetAlert={setAlert} disabled={periodStatus !== PeriodStatuses.DRAFT}/>
+                <CreateButton fields={createFields} objectType={"Expense prediction"} apiUrl={apiUrl} customSetAlert={setAlert} disabled={periodStatus !== PeriodStatuses.DRAFT} />
             </Stack>
             {alert &&
                 <Alert
@@ -90,8 +157,8 @@ const PeriodExpensePredictionsComponent = ({ periodId, periodStatus }) => {
                 >
                     {alert.message}
                 </Alert>}
-            {/* TOTAL SPENT, TOTAL PLANNED, WHAT'S LEFT*/}
-            {/* FILTERS AND SORTERS */}
+            {/* Filters and sorters */}
+            {/* TODO */}
             {periodPredictions.map((prediction) => (
                 <ExpensePredictionCardComponent
                     key={prediction.id}
