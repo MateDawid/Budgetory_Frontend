858aaee1c040bd7bfbecfec77019d6b43c53eb2d
MateDawid <mateusiakdawid@gmail.com>
2025-08-01 08:36:06 +0200

    predictions_in_periods: Use owner and deposit type API fields in Deposit list, detail and form

--- src/entities/components/DepositCard.jsx
+++ src/entities/components/DepositCard.jsx
@@ -1,11 +1,11 @@
-import {Box, Card, CardActions, CardHeader, Stack, Chip, Typography, Divider} from "@mui/material";
+import { Box, Card, CardActions, CardHeader, Stack, Chip, Typography, Divider, Tooltip } from "@mui/material";
 import PageviewIcon from '@mui/icons-material/Pageview';
-import React, {useContext} from "react";
-import {Link} from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from 'react-router-dom';
 import StyledButton from "../../app_infrastructure/components/StyledButton";
 import truncateText from "../../app_infrastructure/utils/truncateText";
 import DeleteButton from "../../app_infrastructure/components/DeleteButton";
-import {BudgetContext} from "../../app_infrastructure/store/BudgetContext";
+import { BudgetContext } from "../../app_infrastructure/store/BudgetContext";
 
 /**
  * DepositCard component to display single Deposit card on Budgets list.
@@ -13,28 +13,45 @@ import {BudgetContext} from "../../app_infrastructure/store/BudgetContext";
  * @param {object} object - Deposit object.
  * @param {function|null} setDeletedObjectId - DepositList useState setter for refreshing Deposit list on Deposit removing.
  */
-const DepositCard = ({apiUrl, object, setDeletedObjectId}) => {
-    const {contextBudgetCurrency} = useContext(BudgetContext);
+const DepositCard = ({ apiUrl, object, setDeletedObjectId }) => {
+    const { contextBudgetCurrency } = useContext(BudgetContext);
 
     return (
-        <Card variant="outlined" sx={{marginTop: 2, borderColor: "#D0D0D0"}}>
-            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <Card variant="outlined" sx={{ marginTop: 2, borderColor: "#D0D0D0" }}>
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                 <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
-                    <CardHeader title={truncateText(object.name, 18)}/>
+                    <CardHeader title={truncateText(object.name, 18)} />
                     <Chip label={object.is_active ? "🟢 Active" : "🔴 Inactive"} variant="outlined" />
                 </Stack>
-                <Divider sx={{width: "100%", mb: 1, mt: 1}}/>
+                <Divider sx={{ width: "100%", mb: 1, mt: 1 }} />
+                <Stack direction="row" alignItems="center" justifyContent="center" spacing={1} width={"100%"}>
+                    <Tooltip title={object.owner_display}>
+                        <Chip label={object.owner_display} variant="outlined" sx={{
+                            maxWidth: '45%',
+                            '& .MuiChip-label': {
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                whiteSpace: 'nowrap'
+                            }
+                        }} />
+                    </Tooltip>
+                    <Tooltip title={object.deposit_type_display}>
+                        <Chip label={object.deposit_type_display} variant="outlined" />
+                    </Tooltip>
+                </Stack>
+                <Divider sx={{ width: "100%", mb: 1, mt: 1 }} />
+
                 <Stack direction="row" alignItems="center" justifyContent="center" spacing={1} width="50%">
                     <Typography fontSize={12} fontWeight="bold" color="secondary">Balance:</Typography>
                     <Typography fontSize={12} color="secondary">{object.balance} {contextBudgetCurrency}</Typography>
                 </Stack>
-                <Divider sx={{width: "100%", mb: 1, mt: 1}}/>
-                <CardActions sx={{width: "100%"}}>
-                    <Box sx={{width: "100%", display: "flex", justifyContent: 'space-around'}}>
-                        <StyledButton component={Link} to={`/deposits/${object.id}`} variant="outlined" startIcon={<PageviewIcon/>}>
+                <Divider sx={{ width: "100%", mb: 1, mt: 1 }} />
+                <CardActions sx={{ width: "100%" }}>
+                    <Box sx={{ width: "100%", display: "flex", justifyContent: 'space-around' }}>
+                        <StyledButton component={Link} to={`/deposits/${object.id}`} variant="outlined" startIcon={<PageviewIcon />}>
                             View
                         </StyledButton>
-                        <DeleteButton apiUrl={apiUrl} objectId={object.id} objectDisplayName="Deposit" setDeletedObjectId={setDeletedObjectId} rightbarDepositsRefresh/>
+                        <DeleteButton apiUrl={apiUrl} objectId={object.id} objectDisplayName="Deposit" setDeletedObjectId={setDeletedObjectId} rightbarDepositsRefresh />
                     </Box>
                 </CardActions>
             </Box>
--- src/entities/pages/DepositList.jsx
+++ src/entities/pages/DepositList.jsx
@@ -1,11 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Typography from "@mui/material/Typography";
-import {Box, Paper, Stack} from "@mui/material";
+import { Box, Paper, Stack } from "@mui/material";
 import Divider from "@mui/material/Divider";
 import Alert from '@mui/material/Alert';
-import {AlertContext} from "../../app_infrastructure/store/AlertContext";
-import {BudgetContext} from "../../app_infrastructure/store/BudgetContext";
-import {getApiObjectsList} from "../../app_infrastructure/services/APIService";
+import { AlertContext } from "../../app_infrastructure/store/AlertContext";
+import { BudgetContext } from "../../app_infrastructure/store/BudgetContext";
+import { getApiObjectsList } from "../../app_infrastructure/services/APIService";
 import DepositCard from "../components/DepositCard";
 import CreateButton from "../../app_infrastructure/components/CreateButton";
 
@@ -14,12 +14,14 @@ import CreateButton from "../../app_infrastructure/components/CreateButton";
  * DepositList component to display list of Budget Deposits.
  */
 export default function DepositList() {
-    const {contextBudgetId, objectChange} = useContext(BudgetContext);
+    const { contextBudgetId, objectChange } = useContext(BudgetContext);
     const apiUrl = `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${contextBudgetId}/deposits/`
-    const {alert, setAlert} = useContext(AlertContext);
+    const { alert, setAlert } = useContext(AlertContext);
     const [updatedObjectId, setUpdatedObjectId] = useState(null);
     const [deletedObjectId, setDeletedObjectId] = useState(null);
     const [objects, setObjects] = useState([]);
+    const [typeOptions, setTypeOptions] = useState([]);
+    const [ownerOptions, setOwnerOptions] = useState([]);
     const createFields = {
         name: {
             type: 'string',
@@ -33,6 +35,20 @@ export default function DepositList() {
             required: false,
             multiline: true,
             rows: 4
+        },
+        deposit_type: {
+            type: 'select',
+            select: true,
+            label: 'Type',
+            required: true,
+            options: typeOptions
+        },
+        owner: {
+            type: 'select',
+            select: true,
+            label: 'Owner',
+            required: false,
+            options: ownerOptions
         },
         is_active: {
             type: 'select',
@@ -50,7 +66,7 @@ export default function DepositList() {
                     label: '🔴 Inactive',
                 }
             ]
-        }
+        },
     }
 
     /**
@@ -62,11 +78,28 @@ export default function DepositList() {
                 const listResponse = await getApiObjectsList(apiUrl)
                 setObjects(listResponse);
             } catch (err) {
-                setAlert({type: 'error', message: "Failed to load Deposits."});
+                setAlert({ type: 'error', message: "Failed to load Deposits." });
             }
         }
         loadData();
     }, [contextBudgetId, objectChange, updatedObjectId, deletedObjectId]);
+
+    /**
+     * Fetches select options for Deposit.owner field from API.
+     */
+    useEffect(() => {
+        const loadOwnerOptions = async () => {
+            try {
+                const ownerResponse = await getApiObjectsList(`${process.env.REACT_APP_BACKEND_URL}/api/budgets/${contextBudgetId}/members/`)
+                setOwnerOptions([{ value: -1, label: '🏦 Common' }, ...ownerResponse]);
+                const typeResponse = await getApiObjectsList(`${process.env.REACT_APP_BACKEND_URL}/api/entities/deposit_types/`)
+                setTypeOptions(typeResponse.results);
+            } catch (err) {
+                setAlert({ type: 'error', message: "Failed to load select fields data." });
+            }
+        }
+        loadOwnerOptions();
+    }, [contextBudgetId]);
 
     return (
         <Paper elevation={24} sx={{
@@ -74,15 +107,15 @@ export default function DepositList() {
         }}>
             <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1} mb={1}>
                 <Typography variant="h4"
-                            sx={{display: 'block', color: '#BD0000'}}>Deposits</Typography>
-                <CreateButton fields={createFields} apiUrl={apiUrl} objectType={"Deposit"}/>
+                    sx={{ display: 'block', color: '#BD0000' }}>Deposits</Typography>
+                <CreateButton fields={createFields} apiUrl={apiUrl} objectType={"Deposit"} />
             </Stack>
-            <Divider/>
-            {alert && <Alert sx={{marginTop: 2, whiteSpace: 'pre-wrap'}} severity={alert.type}
-                             onClose={() => setAlert(null)}>{alert.message}</Alert>}
-            <Box sx={{display: "flex", flexWrap: 'wrap', justifyContent: 'flex-start'}}>
+            <Divider />
+            {alert && <Alert sx={{ marginTop: 2, whiteSpace: 'pre-wrap' }} severity={alert.type}
+                onClose={() => setAlert(null)}>{alert.message}</Alert>}
+            <Box sx={{ display: "flex", flexWrap: 'wrap', justifyContent: 'flex-start' }}>
                 {objects.map(object => (
-                    <Box key={object.id} sx={{width: 330, m: 1}}>
+                    <Box key={object.id} sx={{ width: 330, m: 1 }}>
                         <DepositCard
                             apiUrl={apiUrl}
                             object={object}
