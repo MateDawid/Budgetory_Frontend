738b6d6a589d0ac6dbc5a680d3ae2125358c0db6
MateDawid <mateusiakdawid@gmail.com>
2025-10-03 08:35:05 +0200

    predictions_separate_page: ActivePeriodPrediction row and header

---
+++ src/predictions/components/ExpensePredictionTable/ActivePeriodPredictionRow.jsx
@@ -0,0 +1,57 @@
+import { TableRow, TableCell, Stack, Typography, IconButton, Tooltip } from "@mui/material";
+import React, { useContext } from "react";
+import ColouredLinearProgress from "../../../app_infrastructure/components/CustomLinearProgress/ColouredLinearProgress";
+import { BudgetContext } from "../../../app_infrastructure/store/BudgetContext";
+import EditIcon from '@mui/icons-material/Edit';
+import { getFontColor } from './utils';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+
+export default function ActivePeriodPredictionRow(props) {
+    const { row } = props;
+    const { contextBudgetCurrency } = useContext(BudgetContext);
+
+    const fontColor = getFontColor(row.current_result, row.current_plan);
+
+    return (
+        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+            <TableCell align='center'>{row.category_owner}</TableCell>
+            <TableCell align='center'>{row.category_display}</TableCell>
+            <TableCell align='center'>
+                <Stack direction="row" spacing={1} display="flex" alignItems="center" justifyContent="center">
+                    <Typography variant="body2" color={fontColor}>
+                        {row.current_result}{`\u00A0${contextBudgetCurrency}`} / {row.current_plan}{`\u00A0${contextBudgetCurrency}`}
+                    </Typography>
+                    {row.current_plan !== row.initial_plan &&
+                        <Tooltip title={`Initial prediction value: ${row.initial_plan}\u00A0${contextBudgetCurrency}`} placement="top">
+                            <HelpOutlineIcon />
+                        </Tooltip>
+                    }
+                </Stack>
+            </TableCell>
+            <TableCell align='center' sx={{ color: fontColor }}>{row.current_funds_left}{`\u00A0${contextBudgetCurrency}`}</TableCell>
+            <TableCell align='center'>
+                <Stack gap={1} sx={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    <Typography variant="body2" color={fontColor}>
+                        {row.current_progress}&nbsp;%
+                    </Typography>
+
+                    <ColouredLinearProgress currentValue={row.current_result} maxValue={row.current_plan} />
+                </Stack>
+            </TableCell>
+            <TableCell>
+                <Typography padding={1} variant="body2" sx={{ whiteSpace: 'pre-wrap', textAlign: 'justify', maxWidth: 300}}>
+                    {row.description}
+                </Typography>
+            </TableCell>
+            <TableCell align='right'>
+                <IconButton
+                // onClick={handleEdit}
+                ><EditIcon /></IconButton>
+
+            </TableCell>
+        </TableRow>
+    );
+}
