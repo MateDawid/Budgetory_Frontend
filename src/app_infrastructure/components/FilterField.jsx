eafc4957709aac90a6d5623b1826c2f6111f0a8b
MateDawid <mateusiakdawid@gmail.com>
2025-10-10 07:02:28 +0200

    predictions_separate_page: FilterField accepting optional fields

--- src/app_infrastructure/components/FilterField.jsx
+++ src/app_infrastructure/components/FilterField.jsx
@@ -4,15 +4,21 @@ import React from "react";
 
 /**
  * FilterField component.
- * @param {object} props
- * @param {string} props.label - Label for field.
- * @param {function} props.setFilterValue - Setter for filter value stored in page state.
- * @param {any} props.filterValue - Current value of filter
- * @param {object} props.options - List of select options for field.
- * @param {boolean} [props.disabled] - Handles 'disabled' param of Autocomplete.
- * @param {object} [props.sx] - Additional styling.
+ * @param {{
+ *   label: string; // Label for field.
+ *   setFilterValue: Function; // Setter for filter value stored in page state.
+ *   filterValue: any; // Current value of filter
+ *   options: Array<{label: string; value: any}>; // List of select options for field.
+ *   [key: string]: any; // <= allows extra props like `groupBy`
+ * }} props
  */
-const FilterField = ({ label, setFilterValue, filterValue, options, disabled = false, sx = {} }) => {
+const FilterField = ({ 
+    label, 
+    setFilterValue, 
+    filterValue, 
+    options, 
+    ...props 
+}) => {
     return (
         <Autocomplete
             disablePortal
@@ -21,13 +27,12 @@ const FilterField = ({ label, setFilterValue, filterValue, options, disabled = f
             onChange={(e, selectedOption) => {
                 selectedOption === null ? setFilterValue(null) : setFilterValue(selectedOption.value)
             }}
-            sx={sx}
             renderInput={(params) => {
                 return (<Tooltip title={params.inputProps.value ? params.inputProps.value : undefined} placement="top">
                     <StyledTextField {...params} label={label} sx={{ marginBottom: 1, minWidth: 150 }} />
                 </Tooltip>)
             }}
-            disabled={disabled}
+            {...props}
         />
     )
 }
