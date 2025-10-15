a844ed4dd1e206a4bba26417dad057dd18f8c142
MateDawid <mateusiakdawid@gmail.com>
2025-10-15 07:44:48 +0200

    predictions_separate_page: PredictionInspectModal with table of expenses in period for predicted category

--- src/predictions/pages/ExpensePredictionsPage.jsx
+++ src/predictions/pages/ExpensePredictionsPage.jsx
@@ -251,7 +251,7 @@ export default function ExpensePredictionsPage() {
     }
     else if (periodFilter && periodPredictions.length <= 0) {
         predictionSectionContent = (<Stack alignItems="center" justifyContent="space-between" spacing={1} mb={1}>
-            <Typography color='primary' fontWeight="bold">No predictions found.</Typography>
+            <Typography color='primary' fontWeight="bold">No Predictions found.</Typography>
             {periodStatus === PeriodStatuses.DRAFT &&
                 <CreateButton
                     fields={createFields}
