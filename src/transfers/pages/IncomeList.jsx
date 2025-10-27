f8d8b1c0f06622f4e0d0abb485dd39ced8c3aa5e
MateDawid <mateusiakdawid@gmail.com>
2025-10-27 08:02:50 +0100

    transfers_rework: Better padding on Incomes/Expense pages

--- src/transfers/pages/IncomeList.jsx
+++ src/transfers/pages/IncomeList.jsx
@@ -143,7 +143,7 @@ export default function IncomeList() {
 
     return (
         <>
-            <Paper elevation={24} sx={{ padding: 2, paddingBottom: 0, bgColor: "#F1F1F1", }}>
+            <Paper elevation={24} sx={{ padding: 2, bgColor: "#F1F1F1", }}>
                 <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1} mb={1}>
                     <Typography variant="h4"
                         sx={{ display: 'block', color: '#BD0000' }}>Incomes</Typography>
