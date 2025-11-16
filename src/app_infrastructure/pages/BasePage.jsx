1f31e94abe82b09317b1a5c78ef77b07ff14203c
MateDawid <mateusiakdawid@gmail.com>
2025-11-16 06:21:39 +0100

    transfers_rework: Alert in global Snackbar

--- src/app_infrastructure/pages/BasePage.jsx
+++ src/app_infrastructure/pages/BasePage.jsx
@@ -1,18 +1,20 @@
-import {Outlet, useNavigate} from "react-router-dom";
-import * as React from 'react';
-import {useEffect} from 'react';
+import { Outlet, useNavigate } from "react-router-dom";
+import { useContext, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Navbar from "../components/Navbar";
-import {getAccessToken} from "../../app_users/services/LoginService";
-import {Stack} from "@mui/material";
+import { getAccessToken } from "../../app_users/services/LoginService";
+import { Alert, Snackbar, Stack } from "@mui/material";
 import Leftbar from "../components/Leftbar";
 import Rightbar from "../components/Rightbar";
+import { AlertContext } from "../store/AlertContext";
+import React from "react";
 
 /**
  * Base layout for subpages.
  */
 export default function BasePage() {
-  const navigate = useNavigate();
+    const { alert, setAlert } = useContext(AlertContext);
+    const navigate = useNavigate();
 
     useEffect(() => {
         /**
@@ -28,17 +30,29 @@ export default function BasePage() {
         checkIfTokenExists()
     }, []);
 
-  return (
-      <Box>
-          <Navbar/>
-          <Stack direction="row" justifyContent="space-between" spacing={1}>
-              <Leftbar/>
-              <Box flex={5} pt={2}>
-                  <Outlet/>
-              </Box>
-              <Rightbar/>
-          </Stack>
-      </Box>
-  )
-      ;
+    return (
+        <Box>
+            <Navbar />
+            <Stack direction="row" justifyContent="space-between" spacing={1}>
+                <Leftbar />
+                <Box flex={5} pt={2}>
+                    <Outlet />
+                </Box>
+                <Rightbar />
+            </Stack>
+            <Snackbar
+                open={!!alert}
+                autoHideDuration={8000}
+                onClose={() => setAlert(null)}
+            >
+                <Alert
+                    severity={alert?.type}
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    {alert?.message}
+                </Alert>
+            </Snackbar>
+        </Box>
+    );
 }
