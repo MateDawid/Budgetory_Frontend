88ceaf195728a6ebed346761c1d39665699ca9b3
MateDawid <mateusiakdawid@gmail.com>
2025-12-04 07:08:22 +0100

    landing_page: Plan for Landing Page

--- src/app_infrastructure/pages/LandingPage.jsx
+++ src/app_infrastructure/pages/LandingPage.jsx
@@ -1,4 +1,6 @@
+import { Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
+import BudgetDepositsChart from '../../budgets/components/BudgetDepositsChart';
 
 /**
  * LandingPage component displays home page of application.
@@ -6,7 +8,90 @@ import React from 'react';
 function LandingPage() {
   return (
     <>
-      <h1>LANDING PAGE</h1>
+      <Grid container spacing={2}>
+        <Grid size={12}>
+          <Paper
+            elevation={24}
+            sx={{
+              padding: 2,
+              bgColor: '#F1F1F1',
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{ display: 'block', color: '#BD0000' }}
+            >
+              Period progress bar (basing on current date)<br/>
+              Expenses progress bar<br/>
+              Incomes progress bar
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid size={6}>
+          <Paper
+            elevation={24}
+            sx={{
+              padding: 2,
+              bgColor: '#F1F1F1',
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{ display: 'block', color: '#BD0000' }}
+            >
+              Expenses and incomes in Periods
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid size={6}>
+          <Paper
+            elevation={24}
+            sx={{
+              padding: 2,
+              bgColor: '#F1F1F1',
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{ display: 'block', color: '#BD0000' }}
+            >
+              Deposit results in Periods
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid size={6}>
+          <Paper
+            elevation={24}
+            sx={{
+              padding: 2,
+              bgColor: '#F1F1F1',
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{ display: 'block', color: '#BD0000' }}
+            >
+              Categories and predictions results in Periods
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid size={6}>
+          <Paper
+            elevation={24}
+            sx={{
+              padding: 2,
+              bgColor: '#F1F1F1',
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{ display: 'block', color: '#BD0000' }}
+            >
+              Entity incomes/expenses in Periods
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 }
