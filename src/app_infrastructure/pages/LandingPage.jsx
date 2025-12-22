d7447b0eb8d49737ba6029fdc43f6d5cb5dca1d4
MateDawid <mateusiakdawid@gmail.com>
2025-12-22 07:35:30 +0100

    landing_page: CategoriesInPeriodsChart

--- src/app_infrastructure/pages/LandingPage.jsx
+++ src/app_infrastructure/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import { Grid, Paper, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import DepositsInPeriodsChart from '../../charts/components/DepositsInPeriodsChart';
 import TransfersInPeriodsChart from '../../charts/components/TransfersInPeriodsChart';
+import CategoriesInPeriodsChart from '../../charts/components/CategoriesInPeriodsChart';
 
 const StyledHeader = styled(Typography)(() => ({
   display: 'block',
@@ -52,6 +53,7 @@ function LandingPage() {
             }}
           >
             <StyledHeader variant="h5">Categories in Periods</StyledHeader>
+            <CategoriesInPeriodsChart />
           </Paper>
         </Grid>
         <Grid size={6}>
---
+++ src/charts/components/CategoriesInPeriodsChart.jsx
@@ -0,0 +1,153 @@
+import Stack from '@mui/material/Stack';
+import { LineChart } from '@mui/x-charts/LineChart';
+import { useContext, useEffect, useState } from 'react';
+import React from 'react';
+import { getApiObjectsList } from '../../app_infrastructure/services/APIService';
+import { BudgetContext } from '../../app_infrastructure/store/BudgetContext';
+import FilterField from '../../app_infrastructure/components/FilterField';
+import CategoryTypes from '../../categories/utils/CategoryTypes';
+
+const CATEGORY_TYPE_CHOICES = [
+  { label: 'Expenses', value: CategoryTypes.EXPENSE },
+  { label: 'Incomes', value: CategoryTypes.INCOME },
+];
+
+export default function CategoriesInPeriodsChart() {
+  const { contextBudgetId, contextBudgetCurrency } = useContext(BudgetContext);
+
+  // Selectors choices
+  const [periods, setPeriods] = useState([]);
+  const [deposits, setDeposits] = useState([]);
+  // Filters values
+  const [periodFrom, setPeriodFrom] = useState();
+  const [periodTo, setPeriodTo] = useState();
+  const [categoryType, setCategoryType] = useState(CategoryTypes.EXPENSE);
+  const [deposit, setDeposit] = useState();
+  // Chart data
+  const [xAxis, setXAxis] = useState([]);
+  const [series, setSeries] = useState([]);
+
+  useEffect(() => {
+    const loadPeriodsChoices = async () => {
+      try {
+        const response = await getApiObjectsList(
+          `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${contextBudgetId}/periods/`
+        );
+        setPeriods(response);
+      } catch {
+        setPeriods([]);
+      }
+    };
+    const loadDepositsChoices = async () => {
+      try {
+        const response = await getApiObjectsList(
+          `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${contextBudgetId}/deposits/`
+        );
+        setDeposits(response);
+      } catch {
+        setDeposits([]);
+      }
+    };
+    if (!contextBudgetId) {
+      return;
+    }
+    loadPeriodsChoices();
+    loadDepositsChoices();
+  }, [contextBudgetId]);
+
+  useEffect(() => {
+    const getFilterModel = () => {
+      const filterModel = {};
+      const selectFilters = [
+        { value: periodFrom, apiField: 'period_from' },
+        { value: periodTo, apiField: 'period_to' },
+        { value: deposit, apiField: 'deposit' },
+        { value: categoryType, apiField: 'category_type' },
+      ];
+      selectFilters.forEach((object) => {
+        if (object.value) {
+          filterModel[[object.apiField]] = object.value;
+        }
+      });
+
+      return filterModel;
+    };
+    const loadCategoriesResults = async () => {
+      try {
+        const response = await getApiObjectsList(
+          `${process.env.REACT_APP_BACKEND_URL}/api/budgets/${contextBudgetId}/charts/categories_in_periods/`,
+          {},
+          {},
+          getFilterModel()
+        );
+        const formattedSeries = response.series.map((serie) => ({
+          ...serie,
+          valueFormatter: (value) =>
+            value
+              ? `${value.toString()} ${contextBudgetCurrency}`
+              : `0 ${contextBudgetCurrency}`,
+        }));
+        setXAxis(response.xAxis);
+        setSeries(formattedSeries);
+      } catch {
+        setXAxis([]);
+        setSeries([]);
+      }
+    };
+    if (!contextBudgetId) {
+      return;
+    }
+    loadCategoriesResults();
+  }, [contextBudgetId, periodFrom, periodTo, categoryType, deposit]);
+
+  return (
+    <Stack sx={{ width: '100%' }}>
+      <Stack direction="row" spacing={1} useFlexGap sx={{ flexWrap: 'wrap' }}>
+        <FilterField
+          filterValue={categoryType}
+          setFilterValue={setCategoryType}
+          options={CATEGORY_TYPE_CHOICES}
+          label="Transfer Type"
+          sx={{ width: 160 }}
+          disableClearable
+        />
+        <FilterField
+          options={deposits}
+          label="Deposit"
+          filterValue={deposit || ''}
+          setFilterValue={setDeposit}
+          sx={{ width: 160 }}
+        />
+        <FilterField
+          options={periods}
+          label="Period from"
+          filterValue={periodFrom || ''}
+          setFilterValue={setPeriodFrom}
+          sx={{ width: 160 }}
+        />
+        <FilterField
+          options={periods}
+          label="Period to"
+          filterValue={periodTo || ''}
+          setFilterValue={setPeriodTo}
+          sx={{ width: 160 }}
+        />
+      </Stack>
+      <LineChart
+        xAxis={[{ scaleType: 'band', data: xAxis }]}
+        series={series}
+        height={300}
+        margin={{ bottom: 10 }}
+        slotProps={{
+          legend: {
+            direction: 'horizontal',
+            position: {
+              vertical: 'bottom',
+              horizontal: 'center',
+            },
+          },
+        }}
+      />
+    </Stack>
+  );
+}
